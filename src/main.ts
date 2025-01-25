85bddf185ba15b620d2d9d12e6b56385c288fb4a
I'm Yong Tack <aldjzk@naver.com>
2025-01-25 17:33:08 +0900

    #5 Swagger Add

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Study API Docs')
+    .setDescription('NestJS Study API description')
+    .setVersion('1.0.0')
+    .addTag('swagger')
     .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-
-  // Correct way to set the Swagger UI route to '/api/docs'
-  SwaggerModule.setup('api/docs', app, documentFactory);
-
+  // config를 바탕으로 swagger document 생성
+  const document = SwaggerModule.createDocument(app, options);
+  // Swagger UI에 대한 path를 연결함
+  // .setup('swagger ui endpoint', app, swagger_document)
+  SwaggerModule.setup('api', app, document);
   console.log(`Swagger UI is available at /api/docs`);
+  await app.listen(3000);
 }
 
 bootstrap();
